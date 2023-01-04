705ae76e65c35e34e48401d7d808ff5f4d193341
Victor adriel barbosa pereira <vabpvictor@gmail.com>
2023-01-03 23:05:34 -0300

    iniciando integração com backend

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 
 export default props => {
+
+    const renderRows = () => {
+        const list = props.list || []
+
+        return list.map(todo =>
+        (
+            <tr>
+                <td>{todo.description}</td>
+            </tr>
+        )
+        )
+    }
+
     return (
-        <div>
-            
-        </div>
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
     )
 }
